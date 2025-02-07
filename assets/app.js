d27c45edbcadab9c27261908ac45ae32ab768909
khalil1363 <fray.khalil.200@gmail.com>
2025-02-07 20:33:17 +0100

    Initial Project

---
+++ assets/app.js
@@ -0,0 +1,13 @@
+import './bootstrap.js';
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * This file will be included onto the page via the importmap() Twig function,
+ * which should already be in your base.html.twig.
+ */
+import './styles/app.css';
+import './frontOffice/css/style.css';
+import './frontOffice/css/bootstrap.min.css';
+import './frontOffice/js/main.js';
+
+console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
