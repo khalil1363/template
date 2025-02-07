d83dacfe8e6da5dc862975c695abd5e28fa2d0dc
khalil1363 <fray.khalil.200@gmail.com>
2025-02-07 21:22:38 +0100

    Init Backoffice

--- assets/app.js
+++ assets/app.js
@@ -6,8 +6,6 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
-import './frontOffice/css/style.css';
-import './frontOffice/css/bootstrap.min.css';
-import './frontOffice/js/main.js';
+
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
